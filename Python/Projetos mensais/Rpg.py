61dfdc78d2360a28e96628ef4dbf7308eff1fe70
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-08-07 08:54:53 -0300

    Atributos, níveis e ataque
    Nessa atualização, eu adicionei os atributos base do jogador, o sistema de níveis e o sistema de ataque

--- Python/Projetos mensais/Rpg.py
+++ Python/Projetos mensais/Rpg.py
@@ -1,3 +1,4 @@
+import random
 print('-=-'*35)
 print("Venha jogar um jogo")
 print('-=-'*35)
@@ -12,18 +13,57 @@ classes = {
     3: {'nome': 'Lenhador', 'Força': 1, 'Velocidade': 1, 'Resistência': 3}
 }
 
-print(f'{nome}, escolha uma das 3 classes abaixos\
+nick = input(f'{nome}, escolha um nick de jogador ')
+
+print(f'{nick}, escolha uma das 3 classes abaixos\
       \n1 - Guerreiro: Força[3] Velocidade[1] Resistência [1]\
       \n2 - Algoz: Força[1] Velocidade[3] Resistência[1]\
       \n3 - Lenhador: Força[1] Velocidade[1] Resistência[3]')
-escolha = int(input(f'{nome}, escolha entre 1, 2 e 3 '))
+escolha = int(input(f'{nick}, escolha entre 1, 2 e 3 '))
 
 if escolha in classes:
-    classe = classes[escolha]
-    print(f'{nome}, você escolheu a classe {classe['nome']}\
+      classe = classes[escolha]
+      print(f'\n{nick}, você escolheu a classe {classe['nome']}\
           \nForça = {classe['Força']}\
           \nVelçocidade = {classe['Velocidade']}\
-          \nResistência = {classe['Resistência']}')
-    força = {classe['Força']}
-    veocidade = {classe['Velocidade']}
-    resistência = {classe['Resistência']}
+          \nResistência = {classe['Resistência']}\n')
+      força = classe['Força']
+      velocidade = classe['Velocidade']
+      resistência = classe['Resistência']
+      
+      #Estatos do jogador
+      força_base = força*random.randint(1,3)
+      velocidade_base = velocidade*random.randint(1,3)
+      resistência_base = resistência*random.randint(1,3)
+
+      #Sistema de níveis
+      nível = 1
+      xp = 0
+      xp_up = 10
+      def ganhar_xp(qtd):
+            global xp, nível, xp_up
+            xp += qtd 
+            while xp >= xp_up:
+                  xp -= xp_up
+                  nível += 1
+                  xp_up += 5
+                  print(f"Parabéns! Você subiu para o nível {nível}! ")
+                  print(f'agora seu progresso está assim: {xp} / {xp_up}')
+
+      #Função de ataque
+      def atacar(força_base, velocidade_base):
+            chance_critico = random.randint(1,10)
+            if chance_critico <= velocidade_base:
+                  print('💥 Ataque crítico')
+                  dano = força_base * 2
+            else:
+                  dano = força_base
+            print(f'Você causou {dano} de dano ao inimigo\n')
+            return dano
+      
+      #Fase 1
+
+      
+
+else:
+    print("Opção inválida")
