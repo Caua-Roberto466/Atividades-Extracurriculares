3e03e0dadaf8ec1cb8db486b14189cc4a045f0a4
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-08-17 10:11:41 -0300

    Projeto semana 5 feito
    eu fiz um programa que identifica se os lados formam um triângulo, e qual triângulo eles formam

--- Python/Projeto semanal/projeto_semana5.py
+++ Python/Projeto semanal/projeto_semana5.py
@@ -1,2 +1,19 @@
-salário = float(input('Quanto é seu salário? R$'))
+print("Formador de triângulos")
 
+l1 = float(input("Insira o primeiro lado de um triângulo "))
+l2 = float(input("Insira o segundo lado "))
+l3 = float(input("Insira o terceiro "))
+
+if l1 == l2 and l1 == l3:
+    print(f'O lados {l1}, {l2}, {l3} formão um triângulo')
+    print("O triângulo é equilátero")
+else:
+    if l1 + l2 > l3 and l1 + l3 > l2 and l3 + l2 > l1:
+        print(f'Os lados {l1}, {l2} e {l3} formam um triângulo')
+        if l1 == l2 and l1 != l3:
+            print("O triângulo é isóceles")
+        elif l1 != l2 and l2 != l3:
+            print(f'O triângulo é Escaleno')
+
+    else:
+        print(f'Os lados {l1}, {l2} e {l3} não formam um triângulo')
