7f11bba14217e148905064eb09949b513291e325
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-04 19:50:03 -0300

    Aula 2 de C feita

---
+++ Linguagem C/aprendendo c/aula02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int numero = 13;
+    float numeroQuebrado = 50.49;
+    char letra = 'A';
+    char nome[5] = "Caua";
+
+    printf("Numero: %d \n", numero);
+    printf("Nummero Quebrado: %.2f \n", numeroQuebrado);
+    printf("Letra: %c \n", letra);
+    printf("Nome: %s \n", nome);
+    return 0;
+}
