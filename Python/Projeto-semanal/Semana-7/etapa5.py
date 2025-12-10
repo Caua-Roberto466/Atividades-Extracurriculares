e674ee9b4cb3a7a809f2b6ab611a284d4de50d5f
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-10 19:19:42 -0300

    Etapa 5

--- Python/Projeto-semanal/Semana-7/etapa5.py
+++ Python/Projeto-semanal/Semana-7/etapa5.py
@@ -1,1 +1,17 @@
-import
+import tkinter as tk
+
+janela = tk.Tk()
+janela.title("Etapa 5")
+janela.geometry("400x300")
+
+label = tk.Label(janela, text="Digite uma frase:").pack()
+entrada = tk.Entry(janela)
+entrada.pack()
+
+def exibir():
+    texto_digitado = entrada.get()
+    print("Você digitou: ",texto_digitado)
+
+botao = tk.Button(janela, text="Aperte para exibir", command=exibir).pack()
+
+janela.mainloop()
