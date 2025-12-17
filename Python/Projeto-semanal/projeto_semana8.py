a4223ce7d0214bb9e593628eebfc92eb7651b1c0
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-17 12:32:44 -0300

    Projeto da Semana 8
    O programa monta uma lista de convidados em um arquivo .txt

--- Python/Projeto-semanal/projeto_semana8.py
+++ Python/Projeto-semanal/projeto_semana8.py
@@ -1,1 +1,21 @@
-#Lista de tarefas em arquivo
+#Lista de convidados em arquivo
+while True:
+    print(f'O que deseja fazer? \
+          \n1 - Adicionar convidados \
+          \n2 - Ler os convidados \
+          \n0 - Sair')
+    escolha = int(input("Escolha um número entre 1, 2 ou 0 "))
+
+    if escolha == 1:
+        with open('convidados.txt', 'a') as arquivo:
+            convidado = input("Adicione o convidado: ")
+            arquivo.write(f"Convidado: {convidado}\n")
+    elif escolha == 2:
+        with open('convidados.txt', 'r') as arquivo:
+            ler = arquivo.read()
+            print(f"Convidados \n{ler}")
+    elif escolha == 0:
+        print("Fechando Programa")
+        break
+    else:
+        print("Insira um número válido")
