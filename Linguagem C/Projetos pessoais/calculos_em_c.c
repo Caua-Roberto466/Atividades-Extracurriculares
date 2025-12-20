a7388fd0cd3967476ae957d0d2c331497350c055
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-20 17:41:17 -0300

    tentando mexer em C

---
+++ Linguagem C/Projetos pessoais/calculos_em_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int numero1 = 5;
+    int numero2 = 9;
+
+    float divisao = numero1 / numero2;
+    printf("A divisão de %d por %d é %f", numero1, numero2, divisao);
+    return 0;
+}
