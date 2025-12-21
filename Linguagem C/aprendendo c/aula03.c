c56ab88148ae7f192e52443247eb2e2292a913d1
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-20 21:55:13 -0300

    Aula 3 de C
    Nessa aula eu aprendi a armazenar valores que o usuário digitou

--- Linguagem C/aprendendo c/aula03.c
+++ Linguagem C/aprendendo c/aula03.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    printf("Digite um numero: ")
+    //Declara a variável
+    int numero;
+    printf("Digite um numero inteiro: ");
+    
+    //Pega o valor digitado e armazena na variável numero, precisa do & para armazenar a informação na memória
+    scanf("%d", &numero);
+    printf("O numero foi: %d", numero);
     return 0;
 }
