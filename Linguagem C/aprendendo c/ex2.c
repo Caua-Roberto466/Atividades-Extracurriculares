1f661b94f7499704e02c566aac73c83244e566be
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-22 21:11:39 -0300

    Exercício 2 de C
    Nesse programa você informa o total de gols e o número de partidas e ele faz a média de gols por partida

--- Linguagem C/aprendendo c/ex2.c
+++ Linguagem C/aprendendo c/ex2.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main(){
-    
+    int partidas, total_gols;
+    printf("Digite o total de gols em sua carreira: ");
+    scanf("%d", &total_gols);
+    printf("Digite o total de partidas: ");
+    scanf("%d", &partidas);
+
+    float media = total_gols / partidas;
+    printf("A media de gols por partida e: %0.f", media);
     return 0;
 }
