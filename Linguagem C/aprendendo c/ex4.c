13e3fd117ec9595d125ecd34dbe087f4486881f2
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-22 21:45:27 -0300

    Exercício 4 de C
    Você digita o número de vitórias, empates e derrotas, e o programa retorna o número de pontos

--- Linguagem C/aprendendo c/ex4.c
+++ Linguagem C/aprendendo c/ex4.c
@@ -1,6 +1,15 @@
 //Performace do time
 #include <stdio.h>
 int main(){
-    
+    int vitorias, empates, derrotas;
+    printf("Digite o numeros de vitorias: ");
+    scanf("%d", &vitorias);
+    printf("Digite o numero de empates: ");
+    scanf("%d", &empates);
+    printf("Digite o numero de derrotas: ");
+    scanf("%d", &derrotas);
+    int pontos_vit = vitorias * 3;
+    int total_points = pontos_vit + empates;
+    printf("O total de pontos foi: %d", total_points);
     return 0;
 }
