dd28551cbfd00653e0da377e273c360cf492e2c6
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-26 09:45:24 -0300

    Exercício 8 de C
    Você coloca a quantidade gols que você fez na temporada e ele fala qual foi sua performace

--- Linguagem C/aprendendo c/ex8.c
+++ Linguagem C/aprendendo c/ex8.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
 int main(){
+    int quantidade_gols;
+    printf("Digite a quantidade de gols: ");
+    scanf("%d", &quantidade_gols);
+    if(quantidade_gols < 5){
+        printf("Voce fez uma temporada abaixo do esperado");
+    }else if(quantidade_gols < 11){
+        printf("Voce fez uma boa temporada");
+    }else{
+        printf("Voce fez uma temporada excelente");
+    }
     return 0;
 }
