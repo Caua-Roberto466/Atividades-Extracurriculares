05fcac0a48fe2dcb7a2c04eece5ba6ea1e5252d5
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-12-29 16:27:38 -0300

    Update ex9.c

--- Linguagem C/aprendendo c/ex9.c
+++ Linguagem C/aprendendo c/ex9.c
@@ -1,6 +1,10 @@
-//Exercício 9
+//Analista de gols
 #include <stdio.h>
 int main(){
-  
+  int idade, gols;
+  printf("Digite a sua idade: ");
+  scanf("%d" &idade);
+  printf("Digite a quantidade de gols: ");
+  scanf("%d", &gols)
   return 0;
 }
