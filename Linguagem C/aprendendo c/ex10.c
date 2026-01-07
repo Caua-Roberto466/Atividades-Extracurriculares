0267c9cfa94fd035360525c0f11133251bc209fa
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-01-07 16:54:37 -0300

    Update ex10.c

--- Linguagem C/aprendendo c/ex10.c
+++ Linguagem C/aprendendo c/ex10.c
@@ -2,5 +2,10 @@
 #include <stdio.h>
 int main(){
     int quantidade_max, quantidade_ocupada;
+    printf("Digite a quantidade máxima do estádio: ");
+    scanf("%d", &quantidade_max);
+    printf("Digite a quantidade ocupada no estádio: ");
+    scanf("%d", &quantidade_ocupada);
+    porcentagem = quantidade_ocupada / quantidade_max * 100;
     return 0;
 }
