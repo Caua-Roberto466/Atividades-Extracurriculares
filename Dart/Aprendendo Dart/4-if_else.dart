2fe1295d0cb482eaadd053c9c69bc252851b35b2
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-02-20 20:14:05 -0300

    condições em Dart
    Nessa aula eu aprendi a trabalhar com condições em Dart

---
+++ Dart/Aprendendo Dart/4-if_else.dart
@@ -0,0 +1,44 @@
+void main() {
+  int numero1 = 20;
+  int numero2 = 200;
+
+  print('número 1: $numero1, número 2: $numero2');
+
+  // Caso if, else if e else
+  if (numero1 == numero2) {
+    print('O número 1 é igual ao número 2');
+  } else if (numero1 > numero1) {
+    print('O número 1 é maior que o número 2');
+  } else {
+    print('O número 1 é menor que o número 2');
+  }
+
+  // Caso if e else
+  if (numero1 == numero2) {
+    print('O número 1 é igual ao número 2');
+  } else {
+    print('O número 1 é diferente do número 2');
+  }
+
+  //Caso if solto
+
+  if (numero1 == numero2) {
+    print('numero1 é igual a numero2');
+  }
+
+  if (numero1 > numero2) {
+    print('numero1 é maior que numero2');
+  }
+
+  if (numero1 < numero2) {
+    print('numero1 é menor que numero2');
+  }
+
+  if (numero1 >= numero2) {
+    print('numero1 é maior ou igual a numero2');
+  }
+
+  if (numero1 <= numero2) {
+    print('numero1 é menor ou igual a numero2');
+  }
+}
