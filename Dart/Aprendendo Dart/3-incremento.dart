c4f3696f2c9c7b1b9e7aa9783bd6443da97fce31
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-02-20 19:38:37 -0300

    Incremento em Dart
    Nessa aula eu aprendi a incrementar em Dart

--- Dart/Aprendendo Dart/3-incremento.dart
+++ Dart/Aprendendo Dart/3-incremento.dart
@@ -1,6 +1,20 @@
 void main(){
   int numero;
-  numero = 1;
-
-  print(numero);
+  numero = 10;
+  int numero2 = 10;
+  
+  numero = numero + 1;
+  
+  numero +=1;  // numero = numero + 1
+  
+  numero++;    // numero = numero + 1
+  
+  numero--;    // numero = numero - 1
+  
+  numero -= 2; // numero = numero - 2
+  
+  ++numero;    // numero = numero + 1
+  
+  print(numero++); //Exibe depois soma
+  print(++numero2); //Soma depois exibe
 }
