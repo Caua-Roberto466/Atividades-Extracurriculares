d498bd770ae3aaa35b8fde1c1120139abde48f59
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-02-21 14:14:51 -0300

    Operador ternário
    Nessa aula eu aprendi a usar o operador ternário, que cria um if e else em uma única linha, podendo ser usado para criar variáveis. Tudo na linguagem Dart

---
+++ Dart/Aprendendo Dart/6-operador_ternario.dart
@@ -0,0 +1,17 @@
+void main(){
+  bool condicao = false;
+  String nome = 'Daves';
+  String clima = 'Chuva';
+  
+  //print(condicao ? 'Condição verdadeira' : 'Condição falsa');
+  
+  //print((2 == 3) ? 'menor' : 'maior ou igual');
+  
+  //print(nome == 'Daves' ? 'Nome ok':'nome inválido');
+  
+  //String cliente = nome == 'Daves' ? 'Nome ok':'nome inválido';
+  //print(cliente);
+  
+  String mensagem = clima == 'Sol' ? 'Lindo dia ensolarado':'Dia escuro';
+  print(mensagem);
+}
