e04fd589d3182e1dfe43b5e475728b1c62c2f63e
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-02-25 19:56:02 -0300

    Funções de Par ou Impar e primo
    Criei um programa em que o usuário pode escolher se quer descobrir se o número é primo

--- Dart/Praticando Dart/funcao_par-e-impar_primo.dart
+++ Dart/Praticando Dart/funcao_par-e-impar_primo.dart
@@ -1,5 +1,43 @@
+import 'dart:io';
 void main() {
+  print("Qual função deseja acessar? 1 - Par ou Impar; 2 - Primo ou não");
+  int escolha = int.parse(stdin.readLineSync()!);
 
+  if( escolha == 1){
+    //Par ouy impar
+    stdout.write('Digite um número: ');
+    int numeroParImpar = int.parse(stdin.readLineSync()!);
+    if(par(numeroParImpar)){
+      print('${numeroParImpar} é par');
+    }else{
+      print('Esse número é impar');
+    }
+  }else if(escolha == 2){
+    stdout.write('Digite o número para saber se é primo: ');
+    int numeroPrimo = int.parse(stdin.readLineSync()!);
+    if(primo(numeroPrimo)){
+      print('${numeroPrimo} é prmio');
+    }else{
+      print('Esse número não primo');
+    }
+  }else{
+    print('[ERRO] Digite um número válido');
+  }
 }
 
-bool 
+bool par(int num){
+  if (num % 2 == 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+bool primo(int num){
+  for(int i = 2; i < num; i++){
+    if(num % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
