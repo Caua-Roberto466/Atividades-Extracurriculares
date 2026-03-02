8b5b0ded432427afd07717f91ac37aa706648c2b
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-02 11:36:41 -0300

    Ajustando exercício 53 de python

--- Python/ex_python/ex053.py
+++ Python/ex_python/ex053.py
@@ -1,10 +1,12 @@
 #Ler uma frase qualquer e dizer se ela é um palíndromo, desconsiderando os espaços. (frase que é igual de trás pra frente desconsiderando os espaços) Ex: a torre da derrota
-frase = input("Digite uma frase para saber se ela é um palíndromo: ").lower()
+frase = input("Digite uma frase para saber se ela é um palíndromo: ").upper()
 frase_formatada = frase.replace(" ", "")
 frase_inversa = ""
 
 for char in frase_formatada:
     frase_inversa = char + frase_inversa
+
+print(f'o inverso de {frase_formatada} é {frase_inversa}')
 
 if frase_inversa == frase_formatada:
     print("A frase digitada é um políndromo")
