8d9a4346484e2acc653c46f7466fb995c0fc3dda
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-08 09:00:40 -0300

    Exercício 4 de Kotlin
    Nesse exercício eu corrijo o erro de tipagem das variáveis, trocando de string para int

---
+++ Kotlin/Exercícios/Exercícios_AndroidDevelopers/4-erro_de_tipagem.kt
@@ -0,0 +1,15 @@
+/* Com problema
+fun main() {
+    val numberOfAdults = "20"
+    val numberOfKids = "30"
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total") Saída: The total party size is: 2030
+}
+*/
+//Corrigido
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
