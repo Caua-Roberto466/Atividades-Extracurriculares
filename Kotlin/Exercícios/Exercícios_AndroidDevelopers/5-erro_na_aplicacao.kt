f2e706a39acdbe0c79092cc7f3514c9fb2bc3d9e
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-08 09:12:49 -0300

    Exercício 5 de Kotlin
    Nesse exercício eu Modifico a atribuição da variável do salário total, para somar os dois números

---
+++ Kotlin/Exercícios/Exercícios_AndroidDevelopers/5-erro_na_aplicacao.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = "$baseSalary + $bonusAmount"
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+
+    println("")
+    //Corrigido
+    val totalSalario = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalario (additional bonus).")
+}
