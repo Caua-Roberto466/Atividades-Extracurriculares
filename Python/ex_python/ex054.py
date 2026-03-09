a8c6ac951733cdc795a1b934139ff49d346263d7
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-09 10:11:42 -0300

    Ajustando o exercício 54 de Python

--- Python/ex_python/ex054.py
+++ Python/ex_python/ex054.py
@@ -1,13 +1,14 @@
 #Ler o ano de nascimento de sete pessoas. No final mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores
+from datetime import date
 
 #Inicia as váriaveis
-ano_atual = 2025
+ano_atual = date.today().year
 maior_idade = 0
 menor_idade = 0
 
 #Laço para inserir as 7 idades e calcular quem é menor de idade e quem não é
-for i in range(0, 7):
-    nascimento = int(input(f'Qual o {i+1}º ano de nascimento? '))
+for i in range(1, 8):
+    nascimento = int(input(f'Qual o {i}º ano de nascimento? '))
     if ano_atual - nascimento < 18:
         menor_idade += 1
     else:
