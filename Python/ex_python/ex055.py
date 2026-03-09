d7fe90b071361020d0eea123724d8e6c7cff501f
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-09 10:19:45 -0300

    Ajustando exercício 55 de Pyhton
    Agora não é usado lista no exercício

--- Python/ex_python/ex055.py
+++ Python/ex_python/ex055.py
@@ -1,12 +1,22 @@
 #Ler o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 
-pesos = []
+#pesos = []
+menor = 0
+maior = 0
 
 for i in range(0, 5):
     peso = float(input(f"Adicione {i+1}º peso: "))
-    pesos.append(peso)
+    if i == 1:
+        menor = i
+        maior = i
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+    #pesos.append(peso)
 
-menor = min(pesos)
-maior = max(pesos)
+'''menor = min(pesos)
+maior = max(pesos)'''
 
 print(f'O menor peso registrado é: {menor}Kg e o maior peso é {maior}Kg')
