7aed105414210fb3856635748e8763489751cd1a
Cauã Roberto <cauaroberto211@gmail.com>
2026-04-04 21:08:53 -0300

    header e footer em páginas separadas
    Comecei o teste do header e footer em páginas separadas

---
+++ Site/Site-include/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AAAA</title>
+</head>
+<body>
+    <?php include "header.php"; ?>
+
+    <h1>Página</h1>
+
+    <?php include "footer.php"; ?>
+</body>
+</html>
