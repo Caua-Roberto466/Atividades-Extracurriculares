aab15c6ca337a292f610b9d21ac8613d4a4c0307
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-05-23 20:41:13 -0300

    JSON com Python
    Esse código inicia meu estudo de manipulação de JSON com python

---
+++ Python/programando_python/Python_JSON/feira.py
@@ -0,0 +1,26 @@
+import json
+
+ARQUIVO = "frutas.json"
+
+dados = {
+    'frutas': [
+        {
+            "nome": "Banana",
+            "preço": 2.45
+        },
+        {
+            "nome": "Maçã",
+            "preço": 1.23
+        }
+    ]
+}
+
+with open(ARQUIVO, "w", encoding="utf-8") as arq:
+    json.dump(dados, arq, indent=4, ensure_ascii=False)
+
+frutas = {}
+
+with open(ARQUIVO, "r", encoding='utf-8') as arq:
+    frutas = json.load(arq)
+
+print(frutas)
