292b2f14284eb60a15cdc14ca7c6d4c09581c689
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-05-25 19:29:45 -0300

    Módulos em Python
    Aqui eu aprendi a como trabalhar em projetos com mais de um arquivo só

---
+++ Python/programando_python/Python_modulos_externos/1-Testando-Modulos/contas.py
@@ -0,0 +1,8 @@
+def dobro(n):
+    return n*2
+
+def triplo(n):
+    return n*3
+
+def raiz_quadrada(n):
+    return n**(1/2)
---
+++ Python/programando_python/Python_modulos_externos/1-Testando-Modulos/principal.py
@@ -0,0 +1,7 @@
+import contas as con
+
+num = 25
+
+print(f"O dobro de {num} é {con.dobro(num)}")
+print(f"O triplo de {num} é {con.triplo(num)}")
+print(f"A raiz quadrada de {num} é {con.raiz_quadrada(num)}")
