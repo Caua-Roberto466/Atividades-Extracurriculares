db28bb186d34e5e74ecbb105583eb0c36843f485
Caua-Roberto466 <cauaroberto211@gmail.com>
2026-06-03 20:27:48 -0300

    Opção de cadastrar produto

--- Python/programando_python/Python_list_comprehension/1-estoque_loja/main.py
+++ Python/programando_python/Python_list_comprehension/1-estoque_loja/main.py
@@ -1,9 +1,41 @@
-import historico as hst
+from historico import carregar, salvar
 
 def menu():
     print("")
     print("-="*25)
     print("O que deseja fazer?")
     print("1 - Cadastrar produto")
-    print("")
+    print("2 - Buscar produto por categoria")
+    print("3 - Ordenar por preço")
+    print("4 - Calcular valor do estoque")
+    print("5 - Relatório final")
+    print("0 - Sair")
     print("-="*25)
+
+historico = carregar()
+
+while True:
+    menu()
+    
+    try:
+        escolha = int(input("O que fazer? "))
+    except ValueError:
+        print("Digite um número")
+    else:
+        if escolha == 1:
+            nome = input("\nDigite o nome do produto: ")
+            preco = float(input("Digite preço do produto: "))
+            categoria = input("Digite a categoria do produto: ")
+            quantidade = int(input("Digite a quantidade do produto: "))
+            
+            produto = {
+                'nome': nome,
+                'preço': preco,
+                'categoria': categoria,
+                'quantidade': quantidade
+            }
+
+            historico.append(produto)
+            salvar(historico)
+        elif escolha == 2:
+            pass
